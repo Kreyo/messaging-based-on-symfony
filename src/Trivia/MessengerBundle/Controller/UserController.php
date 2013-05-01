2c317980b178c20378ae1685d584a885d9ae3de0
Kreyo <matthew.timoshenko@gmail.com>
2013-05-01 14:08:30 +0300

    issue #8.5
    fixed the whole registration thing.
    user is now able to register, and after registration - to look at the
    creepy main page.

--- src/Trivia/MessengerBundle/Controller/UserController.php
+++ src/Trivia/MessengerBundle/Controller/UserController.php
@@ -52,7 +52,7 @@ class UserController extends Controller{
                 $em->flush();
 
                 $automessage = new Messages();
-                $automessage->setToUser($user);
+                $automessage->setToUser($user->getUsername());
 
                 $automessage->setFromUser(null);
                 $automessage->setText('Welcome to our neat little messaging system, ' . $user->getUsername() );
