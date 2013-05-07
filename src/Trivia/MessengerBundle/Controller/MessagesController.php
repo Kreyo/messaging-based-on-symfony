323a16fd8c30e8d95b2b56ffddc2be0020c3cc4a
Kreyo <matthew.timoshenko@gmail.com>
2013-05-07 12:50:07 +0300

    issue # 9.8
    view - added, working
    sent and new messages added
    new message creation - still not working

--- src/Trivia/MessengerBundle/Controller/MessagesController.php
+++ src/Trivia/MessengerBundle/Controller/MessagesController.php
@@ -37,14 +37,15 @@ class MessagesController extends Controller
             ->add('toUser', 'text')
             ->add('text', 'textarea')
             ->getForm();
+        $form->bind($this->getRequest());
         if ($this->getRequest()->isMethod('POST')) {
 
 
-            $message->setText($form["text"]->getData());
+            $message->setText($this->getRequest()->get('text'));
             $message->setFromUser($this->getUser());
             $message->setUnread();
             $message->setToUser($this->getDoctrine()->getManager()->getRepository('TriviaMessengerBundle:Users')->findOneByUsername($this->getRequest()->get('toUser')));
-            $form->bind($this->getRequest());
+
 
             if ($form->isValid()) {
                 $em = $this->getDoctrine()->getManager();
@@ -59,16 +60,56 @@ class MessagesController extends Controller
     }
 
     public function viewAction($id){
-        $message = $this->getRepository('TriviaMessengerBundle:Messages')->findOneById($this->getRequest()->get('id'));
+        $message = $this->getDoctrine()->getManager()->getRepository('TriviaMessengerBundle:Messages')->findOneById($this->getRequest()->get('id'));
 
         if (!$message) {
             throw $this->createNotFoundException('Message not found');
         }
-        if($this->getUser()->getUsername() == $message->getRecipient()){
+        if($this->getUser()->getUsername() == $message->getToUser()->getUsername()){
             $message->setRead();
             $this->getDoctrine()->getManager()->flush();
         }
-        return $this->render('TriviaMessengerBundle :Messenger:view.html.twig', array('message' => $message));
+        return $this->render('TriviaMessengerBundle:Messenger:view.html.twig', array('message' => $message));
     }
+    public function newAction()
+    {
+        $messages = $this->get('doctrine')->getManager()
+            ->createQueryBuilder()
+            ->select('m')
+            ->from('TriviaMessengerBundle:Messages', 'm')
+            ->where('m.is_read = :false AND m.fromUser = :user OR m.toUser = :user AND m.is_read = :false')
+            ->setParameters(array(
+                            'user' => $this->getUser(),
+                            'false'=> false,
+            ))
 
+            ->getQuery()
+            ->getResult();
+        $index_paginator = $this->get('knp_paginator');
+        $pagination = $index_paginator->paginate(
+            $messages,
+            $this->get('request')->query->get('page', 1),
+            10/*limit per page*/
+        );
+        return $this->render('TriviaMessengerBundle:Messenger:new.html.twig', array('pagination' => $pagination ));
+    }
+    public function sentAction()
+    {
+        $messages = $this->get('doctrine')->getManager()
+            ->createQueryBuilder()
+            ->select('m')
+            ->from('TriviaMessengerBundle:Messages', 'm')
+            ->where('m.fromUser = :user')
+            ->setParameter('user', $this->getUser())
+            ->getQuery()
+            ->getResult();
+        $index_paginator = $this->get('knp_paginator');
+        $pagination = $index_paginator->paginate(
+            $messages,
+            $this->get('request')->query->get('page', 1),
+            10/*limit per page*/
+        );
+
+        return $this->render('TriviaMessengerBundle:Messenger:sent.html.twig', array('pagination' => $pagination ));
+    }
 }
